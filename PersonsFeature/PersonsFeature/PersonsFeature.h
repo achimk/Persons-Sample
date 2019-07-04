73ccdfbc71c0c968ba5a304ba55045f4c5c16c63
Joachim Kret <joachimkret@macbook.pro>
2019-07-04 08:39:16 +0200

    Prepared modules.

---
+++ PersonsFeature/PersonsFeature/PersonsFeature.h
@@ -0,0 +1,19 @@
+//
+//  PersonsFeature.h
+//  PersonsFeature
+//
+//  Created by Joachim Kret on 04/07/2019.
+//  Copyright © 2019 Joachim Kret. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PersonsFeature.
+FOUNDATION_EXPORT double PersonsFeatureVersionNumber;
+
+//! Project version string for PersonsFeature.
+FOUNDATION_EXPORT const unsigned char PersonsFeatureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PersonsFeature/PublicHeader.h>
+
+
